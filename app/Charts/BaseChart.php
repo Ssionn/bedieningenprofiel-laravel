d9840b5bcedc17ded075b810ee806d880b8fa4d7
Ssionn <casper.kizewski.developer@gmail.com>
2025-03-10 19:02:31 +0100

    Added ChartJS (WIP) for charts

---
+++ app/Charts/BaseChart.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Charts;
+
+use IcehouseVentures\LaravelChartjs\Builder;
+
+abstract class BaseChart
+{
+    /**
+    * Returns the ChartJS Builder. This creates the chart so make sure to have data ready!
+    */
+    abstract public function createChart(): Builder;
+
+    /**
+    * Returns colors for a specific chart.
+    */
+    public function colors(array $color): array
+    {
+        return $color;
+    }
+}
---
+++ app/Charts/TeamRoleDistributionChart.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Charts;
+
+use App\Repositories\ChartRepository;
+use IcehouseVentures\LaravelChartjs\Builder;
+use IcehouseVentures\LaravelChartjs\Facades\Chartjs;
+
+class TeamRoleDistributionChart extends BaseChart
+{
+    const chartTitle = "Total score for all roles";
+
+    public function __construct(
+        protected ChartRepository $chartRepository
+    ) {
+    }
+
+    public function createChart(): Builder
+    {
+        $data = $this->chartRepository->getTeamRoleDistributionScores();
+        $labels = [];
+        $datasets = [
+            'label' => self::chartTitle,
+            'data' => []
+        ];
+
+        // This is for five people, later this is substituted for the amount of people in a team.
+        $personScores = [0, 0, 0, 0, 0];
+
+        foreach ($data as $value) {
+            $labels[] = $value['role'];
+
+            $roleTotal = array_sum($value['scores']);
+            $datasets['data'][] = $roleTotal;
+
+            foreach ($value['scores'] as $index => $score) {
+                $personScores[$index] += $score;
+            }
+        }
+
+        return Chartjs::build()
+            ->name('TeamRoleDistributionChart')
+            ->type('bar')
+            ->size(['width' => 400, 'height' => 200])
+            ->labels($labels)
+            ->datasets([$datasets])
+            ->options($this->options());
+    }
+
+    public function options(string $title = 'Placeholder', bool $display = false): array
+    {
+        return [
+            'scales' => [
+                'y' => [
+                    'suggestedMax' => 200
+                ],
+            ],
+            'plugins' => [
+                'title' => [
+                    'display' => $display,
+                    'text' => $title
+                ]
+            ]
+        ];
+    }
+}
